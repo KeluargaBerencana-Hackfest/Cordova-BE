9feecb3ff2a3d57d9b4cc370725086e3e78bdf25
Gede Indra Adi Brata <indrabrata599@gmail.com>
2024-01-15 16:48:55 +0800

    `Update:` main

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"log"
 	"os"
 	"sync"
 
 	"github.com/Ndraaa15/cordova/app"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load("./.env"); err != nil {
-		log.Printf("[cordova-main] failed to load .env file. Error : %v\n", err)
-		return
-	}
+	// if err := godotenv.Load("./.env"); err != nil {
+	// 	log.Printf("[cordova-main] failed to load .env file. Error : %v\n", err)
+	// 	return
+	// }
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
